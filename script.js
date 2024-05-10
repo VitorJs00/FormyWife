c3ea37f8a41e38e56e3f036725605afe668abf6f
VitorJs00 <161177573+VitorJs00@users.noreply.github.com>
2024-05-09 23:02:02 -0300

    feat: PONDO ALERTS

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let fugitivo = document.getElementsByClassName('MALDITO')[0]
 
 fugitivo.addEventListener('mouseover',(e)=>{
-    
+
     if(e){
         
+        fugitivo.style.backgroundColor = "green"
+        alert('EU TE AMO MUITO IARA')
         fugitivo.style.top= pula()+'px'
         fugitivo.style.left = pula()+'px'
         
@@ -13,4 +15,11 @@ function pula(){
 let aleatoryNum = Math.floor(Math.random()* (300-100) + 10)
 return aleatoryNum
 }
+})
+
+fugitivo.addEventListener('click',(e)=>{
+    if(e){
+        alert('INSSISTENTE CHATAAAAAAAA')
+        fugitivo.style.backgroundColor = "red"
+    }
 })
